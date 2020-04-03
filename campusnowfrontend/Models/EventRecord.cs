ed33aa8be8a220f411b56f8475310641456da1ad
warthogs32 <jason.jen@weatherford.com>
2020-04-03 01:29:34 -0700

    get all events

---
+++ campusnowfrontend/Models/EventRecord.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campusnowfrontend.Models
+{
+    public class EventRecord
+    {
+        private int _listingId;
+        public int ListingId
+        {
+            get => _listingId;
+            set => _listingId = value;
+        }
+
+        private int _userId;
+        public int UserId
+        {
+            get => _userId;
+            set => _userId = value;
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+
+        private DateTime _startTime;
+        public DateTime StartTime
+        {
+            get => _startTime;
+            set => _startTime = value;
+        }
+
+        private DateTime _endTime;
+        public DateTime EndTime
+        {
+            get => _endTime;
+            set => _endTime = value;
+        }
+
+        private double _locX;
+        public double LocX
+        {
+            get => _locX;
+            set
+            {
+                if (value >= -180.0 && value <= 180.0)
+                {
+                    _locX = value;
+                }
+            }
+        }
+
+        private double _locY;
+        public double LocY
+        {
+            get => _locY;
+            set
+            {
+                if (value >= -180.0 && value <= 180.0)
+                {
+                    _locY = value;
+                }
+            }
+        }
+    }
+}
---
+++ campusnowfrontend/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using campusnowfrontend.Models;
+using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using campusnowfrontend.Views;
+using Prism.Commands;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace campusnowfrontend.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public ICommand ShowCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            ShowCommand = new DelegateCommand(ShowMethod);
+        }
+
+        private void ShowMethod()
+        {
+            LoginView login = new LoginView();
+            login.Show();
+        }
+
+        private string _server;
+
+        public string Server
+        {
+            get { return _server; }
+            set 
+            { 
+                _server = value; 
+            }
+        }
+
+        private ObservableCollection<EventRecord> _allEvents = new ObservableCollection<EventRecord>();
+        public ObservableCollection<EventRecord> AllEvents
+        {
+            get 
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(_server);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("api/event/getAllEvents").Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    var result = JObject.Parse(response.Content.ReadAsStringAsync().Result)["EventRecords"].Children().ToList();
+                    result.ForEach(x => _allEvents.Add(x.ToObject<EventRecord>()));
+                }
+
+                return _allEvents;
+            }
+        }
+
+
+    }
+}
